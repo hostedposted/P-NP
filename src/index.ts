ca5debeaca2b3ef2c467630f0f4c1d26c909fc5d
ArcerionDev <arcerion114@gmail.com>
2022-01-30 20:11:32 -0500

    fixing

--- src/index.ts
+++ src/index.ts
@@ -20,70 +20,137 @@ const unminifySource = false;
 		next()
 	})
 
-	app.get(/\/(api\/)?game.min.js/, async (req, res) => {
-		if (req.query.version && typeof req.query.version !== "string")
-			return res.status(400).send("Invalid version specified.");
-		const version = req.query.version ?? gs.gameClientVersion;
-		try {
-			res.type("js").send(`// game.min.js v${version}\n\n`+
-				(unminifySource ? beautify : (_: any) => _)
-					(await getPatchedGameFile(version))
-			);
-		} catch (e: unknown) {
-			if (!(e instanceof Error)) throw e;
-			return res.status(400).send(e.message);
+	let toAdd = []
+	let cache = JSON.parse(fs.readFileSync('./hits.json', 'utf8'))
+	let validate = (a: any, b: any, type: any) => {
+
+		switch (type) {
+
+			case "day":
+				return a.getFullYear() === b.getFullYear() &&
+					a.getMonth() === b.getMonth() &&
+					a.getDate() === b.getDate();
+
+				break;
+
+			case "week":
+				return a.getFullYear() === b.getFullYear() &&
+					a.getMonth() === b.getMonth() &&
+					Math.ceil((a.getDate() - 1 - a.getDay()) / 7) === Math.ceil((b.getDate() - 1 - b.getDay()) / 7);
+				break;
+			case "month":
+				return a.getFullYear() === b.getFullYear() &&
+					a.getMonth() === b.getMonth()
+				break;
 		}
-	});
-	app.get(/\/(api\/)?public-game.min.js/, async (req, res) => {
-		if (typeof req.query.hash !== "string")
-			return res.status(400).send("No hash specified.");
-		try {
-			res.type("js").send(await getPatchedPublicGameFile(req.query.hash));
-		} catch (e: unknown) {
-			if (!(e instanceof Error)) throw e;
-			return res.status(400).send(e.message);
-		}
-	});
 
-	app.get("/version", (req, res) => res.send(VERSION));
-	app.get("/download", (req, res) => res.redirect(DOWNLOAD_LINK));
-	app.post("/hit", (req, res) => {
-		let current = { "ip": req.ip, timestamp: Date.now() }
-		let data = JSON.parse(fs.readFileSync('./hits.json', 'utf8'))
-		data.push(current)
-		fs.writeFileSync('./hits.json', JSON.stringify(data))
-		res.status(200)
-		res.send({ "status": "success", "data": current })
-	});
-	app.get("/stats", (req, res) => {
-		let data = JSON.parse(fs.readFileSync('./hits.json', 'utf8'))
 
-		let validate = (a: any, b: any, type: any) => {
+	}
+	stats = {
 
-			switch (type) {
+		count: data.length,
+		uniques: [...new Set(data.flatMap(({ ip }: { ip: any }) => ip))].sort().length,
+		timeData: {
 
-				case "day":
-					return a.getFullYear() === b.getFullYear() &&
-						a.getMonth() === b.getMonth() &&
-						a.getDate() === b.getDate();
 
-					break;
+			recent: ["day", "week", "month"].map((y) => { return [y, { count: data.map((x) => { if (validate(new Date(), new Date(x.timestamp), y)) { return x; } }).filter(Boolean).length, uniques: [...(data.map((x) => { if (validate(new Date(), new Date(x.timestamp), y)) { return x } }).filter(Boolean)).map(x => x.ip).reduce((acc, e) => acc.set(e, (acc.get(e) || 0) + 1), new Map()).keys()].length },]; }).reduce((o, v, i) => { return (o[v[0]] = v.slice(1)[0]), o; }, {}),
+			analysis: {
 
-				case "week":
-					return a.getFullYear() === b.getFullYear() &&
-						a.getMonth() === b.getMonth() &&
-						Math.ceil((a.getDate() - 1 - a.getDay()) / 7) === Math.ceil((b.getDate() - 1 - b.getDay()) / 7);
-					break;
-				case "month":
-					return a.getFullYear() === b.getFullYear() &&
-						a.getMonth() === b.getMonth()
-					break;
+				day: new Array(14)
+					.fill()
+					.map((a, index) => {
+
+						let date = new Date()
+						let month = date.getMonth() + 1
+						let day = date.getDate() - index
+						let year = date.getFullYear()
+						if (day < 1) {
+							day = new Date(date.getYear(), date.getMonth(), 0).getDate() + day;
+							--month
+						}
+						if (month < 1) {
+
+							month = 12;
+							year--
+						}
+						return { month: month, day: day, year: year }
+					}).reverse().map(x => {
+
+						return [
+							`${x.month}/${x.day}`,
+							{
+
+								count: data.filter((y) => {
+									return (
+										new Date(y.timestamp).getFullYear() === x.year &&
+										new Date(y.timestamp).getMonth() + 1 === x.month &&
+										new Date(y.timestamp).getDate() === x.day
+									);
+								}).length, 
+								uniques: 
+								[...new Set(data.filter((y) => {
+									return (
+										new Date(y.timestamp).getFullYear() === x.year &&
+										new Date(y.timestamp).getMonth() + 1 === x.month &&
+										new Date(y.timestamp).getDate() === x.day
+									);
+								}).flatMap(({ ip }: { ip: any }) => ip))].sort().length
+							},
+						];
+
+					}),
+				
+				month: new Array(12)
+					.fill()
+					.map((a, index) => {
+
+						let date = new Date()
+						let month = date.getMonth() - (index - 1)
+						let year = date.getFullYear()
+						if (month < 1) {
+
+							month = 12 + month
+							--year
+						}
+						return { month: month, year: year }
+					}).reverse().map(x => {
+
+						return [
+							`${['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][x.month - 1]} ${x.year}`,
+							{
+
+								count: data.filter((y) => {
+									return (
+										new Date(y.timestamp).getFullYear() === x.year &&
+										new Date(y.timestamp).getMonth() + 1 === x.month
+
+									);
+								}).length,
+								uniques: [...new Set(data.filter((y) => {
+									return (
+										new Date(y.timestamp).getFullYear() === x.year &&
+										new Date(y.timestamp).getMonth() + 1 === x.month
+
+									);
+								}).flatMap(({ ip }: { ip: any }) => ip))].sort().length
+
+							}
+						];
+
+					})
+
+
 			}
-
-
 		}
+	}
+	
 
-		res.send({
+	setInterval(() => {
+		cache = JSON.parse(fs.readFileSync('./hits.json', 'utf8'))
+		fs.writeFileSync('./hits.json', JSON.stringify(cache.slice(0).concat(toAdd)))
+		toAdd = []
+		let data = cache
+		stats = {
 
 			count: data.length,
 			uniques: [...new Set(data.flatMap(({ ip }: { ip: any }) => ip))].sort().length,
@@ -179,7 +246,44 @@ const unminifySource = false;
 
 				}
 			}
-		})
+		}
+	},30*60*1000)
+
+	app.get(/\/(api\/)?game.min.js/, async (req, res) => {
+		if (req.query.version && typeof req.query.version !== "string")
+			return res.status(400).send("Invalid version specified.");
+		const version = req.query.version ?? gs.gameClientVersion;
+		try {
+			res.type("js").send(`// game.min.js v${version}\n\n`+
+				(unminifySource ? beautify : (_: any) => _)
+					(await getPatchedGameFile(version))
+			);
+		} catch (e: unknown) {
+			if (!(e instanceof Error)) throw e;
+			return res.status(400).send(e.message);
+		}
+	});
+	app.get(/\/(api\/)?public-game.min.js/, async (req, res) => {
+		if (typeof req.query.hash !== "string")
+			return res.status(400).send("No hash specified.");
+		try {
+			res.type("js").send(await getPatchedPublicGameFile(req.query.hash));
+		} catch (e: unknown) {
+			if (!(e instanceof Error)) throw e;
+			return res.status(400).send(e.message);
+		}
+	});
+
+	app.get("/version", (req, res) => res.send(VERSION));
+	app.get("/download", (req, res) => res.redirect(DOWNLOAD_LINK));
+	app.post("/hit", (req, res) => {
+		let current = { "ip": req.ip, timestamp: Date.now() }
+		toAdd.push(current)
+		res.status(200)
+		res.send({ "status": "success", "data": current })
+	});
+	app.get("/stats", (req, res) => {
+		res.send(stats)
 	});
 	
 	const addr: ReturnType<Server["address"]> = app.listen(process.env.PORT ?? 1337, () =>
